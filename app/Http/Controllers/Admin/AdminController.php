21aa30e605a09a3e02016237211d2e4418ab3d80
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-06-11 01:01:28 +0600

    Update admin Controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Doctor;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
@@ -19,12 +20,19 @@ class AdminController extends Controller
     public function Logout()
     {
         Auth::logout();
-        return redirect()->route('welcome');
+        return redirect()->route('login');
     }
 
     public function alluser()
     {
         $users = User::latest()->get();
         return view('admin.alluser',compact('users'));
+    }
+    public function deleteu($user_id)
+    {
+
+        User::find($user_id)->delete();
+        Toastr::success('User successfuly Deleted');
+        return Redirect()->back();
     }
 }
