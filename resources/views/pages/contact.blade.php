935a3c36c738eeecba12c0ecb5e1ed47dd8fe271
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-06-13 00:43:56 +0600

    Create contact.blade.php

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.front_layouts')
+@section('contact') active @endsection
+@section('content')
+<section class="page-title bg-1">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="block text-center">
+            <span class="text-white">Contact Us</span>
+            <h1 class="text-capitalize mb-5 text-lg">Get in Touch</h1>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- contact form start -->
+
+  <section class="section contact-info pb-0">
+      <div class="container">
+           <div class="row">
+              <div class="col-lg-4 col-sm-6 col-md-6">
+                  <div class="contact-block mb-4 mb-lg-0">
+                      <i class="icofont-live-support"></i>
+                      <h5>Call Us</h5>
+                       +823-4565-13456
+                  </div>
+              </div>
+              <div class="col-lg-4 col-sm-6 col-md-6">
+                  <div class="contact-block mb-4 mb-lg-0">
+                      <i class="icofont-support-faq"></i>
+                      <h5>Email Us</h5>
+                       contact@mail.com
+                  </div>
+              </div>
+              <div class="col-lg-4 col-sm-6 col-md-6">
+                  <div class="contact-block mb-4 mb-lg-0">
+                      <i class="icofont-location-pin"></i>
+                      <h5>Location</h5>
+                       North Main Street,Brooklyn Australia
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section class="contact-form-wrap section" >
+      <div class="container" style="background-color: rgb(212, 193, 193)">
+          <div class="row justify-content-center">
+              <div class="col-lg-6">
+                  <div class="section-title text-center">
+                      <h2 class="text-md mb-2">Contact us</h2>
+                      <div class="divider mx-auto my-4"></div>
+                      <p class="mb-5"></p>
+                  </div>
+              </div>
+          </div>
+          <div class="row" >
+              <div class="col-lg-12 col-md-12 col-sm-12" >
+                  <form id="contact-form" class="contact__form " method="post" action="mail.php" >
+                   <!-- form message -->
+                      <div class="row">
+                          <div class="col-12">
+                              <div class="alert alert-success contact__msg" style="display: none" role="alert">
+                                  Your message was sent successfully.
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="row" >
+                          <div class="col-lg-6">
+                              <div class="form-group">
+                                  <input name="name" id="name" type="text" class="form-control" placeholder="Your Full Name" >
+                              </div>
+                          </div>
+
+                          <div class="col-lg-6">
+                              <div class="form-group">
+                                  <input name="email" id="email" type="email" class="form-control" placeholder="Your Email Address">
+                              </div>
+                          </div>
+                           <div class="col-lg-6">
+                              <div class="form-group">
+                                  <input name="subject" id="subject" type="text" class="form-control" placeholder="Your Query Topic">
+                              </div>
+                          </div>
+                           <div class="col-lg-6">
+                              <div class="form-group">
+                                  <input name="phone" id="phone" type="text" class="form-control" placeholder="Your Phone Number">
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="form-group-2 mb-4">
+                          <textarea name="message" id="message" class="form-control" rows="8" placeholder="Your Message"></textarea>
+                      </div>
+
+                      <div class="text-center">
+                          <input class="btn btn-main btn-round-full" name="submit" type="submit" value="Send Messege"></input>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </section>
+
+@endsection
